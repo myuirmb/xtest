996ad8aed659428dea4ec9b5916b0b2a202625d5
randy <myuirmb@gmail.com>
2019-12-03 18:28:06 +0800

    add long touch event

--- xwebb/src/components/menu/MainMenu.vue
+++ xwebb/src/components/menu/MainMenu.vue
@@ -4,8 +4,9 @@
 <template>
   <van-tabbar route v-model="active">
     <!--  @change="onChange" -->
+    <!-- <div  style="width:50px;height:50px;background:red;"></div> -->
     <van-tabbar-item name="home" icon="wap-home-o" to="/home">首页</van-tabbar-item>
-    <van-tabbar-item name="contact" icon="friends-o" to="/contact">联系人</van-tabbar-item>
+    <van-tabbar-item name="contact" icon="friends-o" to="/contact" v-longTouch="handleLongTouch">联系人</van-tabbar-item>
     <van-tabbar-item name="message" icon="chat-o" to="/message" info>消息</van-tabbar-item>
     <van-tabbar-item name="lifes" icon="flower-o" to="/lifes">生活</van-tabbar-item>
     <van-tabbar-item name="my" icon="manager-o" to="/my">我的</van-tabbar-item>
@@ -15,9 +16,11 @@
 <script>
 import { Tabbar, TabbarItem } from "vant";
 import { mapMutations } from "vuex";
+import longTouch from "./LongTouch.js";
 
 export default {
   name: "MainMenu",
+  directives: { longTouch },
   components: {
     [Tabbar.name]: Tabbar,
     [TabbarItem.name]: TabbarItem
@@ -44,11 +47,14 @@ export default {
   // },
   // mounted() {},
   methods: {
-    ...mapMutations(["setActive"])
+    ...mapMutations(["setActive"]),
     // onChange(active) {
     //   // console.log(active);
     //   // this.setActive({active})
     // }
+    handleLongTouch() {
+      console.log(arguments);
+    }
   }
 };
 </script>
